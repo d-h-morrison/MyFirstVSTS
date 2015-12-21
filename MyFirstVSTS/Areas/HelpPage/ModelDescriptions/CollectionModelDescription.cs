65a214e3df29b2cc6b3ebca4d50bff8f4fe62094
d-h-morrison <d_h_morrison@outlook.com>
2015-12-21 17:00:37 -0500

    first check in ever

---
+++ MyFirstVSTS/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MyFirstVSTS.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MyFirstVSTS/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyFirstVSTS.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
